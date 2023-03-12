b09b312e2d1f073c93984d5565f4bf3ecfad587a
Blacktrecks <107044395+Blacktrecks@users.noreply.github.com>
2023-03-12 21:07:27 +0200

    Update Clase_proiect.cs

--- Clase_proiect.cs
+++ Clase_proiect.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 
@@ -7,7 +7,7 @@ namespace Aplicatie_de_Gestiune_a_Unui_Magazin_de_Biciclete
 {
    //Clasa Bicicleta reprezinta bicicleta cu urmatoarele caracteristici: brand, model, pret si disponibilitate pe stoc
 
-   class Bicicleta
+  public class Bicicleta
    {
       public string Brand { get; set; }
       public string Model { get; set; }
@@ -26,7 +26,7 @@ namespace Aplicatie_de_Gestiune_a_Unui_Magazin_de_Biciclete
       //Supradefinim si returnam variabilele clasei
       public override string ToString()
       {
-         return $"Brand: {Brand}, Model: {Model}, Pret: {Pret:C}, Available: {(Stoc ? "DA" : "NU")}";
+         return $"Brand: {Brand}, Model: {Model}, Pret: {Pret:C}RON, Disponibilitate: {(Stoc ? "DA" : "NU")}";
       }
 
       // Clasa MagazinBicicleta reprezinta un magazin online ce vinde biciclete
@@ -34,6 +34,15 @@ namespace Aplicatie_de_Gestiune_a_Unui_Magazin_de_Biciclete
       public class MagazinBicicleta
       {
          private List<Bicicleta> _biciclete = new List<Bicicleta>(); //declaram o noua lista privata ce va contine bicicletele
+         public List<Bicicleta> Inventar { get; set; }
+         public List<Client> Clienti { get; set; }
+
+         public MagazinBicicleta()
+         { 
+               Inventar = new List<Bicicleta>();
+               Clienti = new List<Client>();
+            
+         }
 
          //Adauga bicicleta 
          public void AdaugaBicicleta(Bicicleta bicicleta)
@@ -60,15 +69,86 @@ namespace Aplicatie_de_Gestiune_a_Unui_Magazin_de_Biciclete
                }
             }
             return bicicleteDisponibile.Count > 0 ? bicicleteDisponibile : new List<Bicicleta>();
-            
+
          }
+
+         //adauga bicicleta in magazin
+         public void AddBicicleta(Bicicleta bicicleta)
+         {
+            Inventar.Add(bicicleta);
+         }
+
+         //sterge bicicleta din magazin
+         public void StergeBicicleta(Bicicleta bicicleta)
+         {
+            Inventar.Remove(bicicleta);
+         }
+
+         //Adauga client in magazin
+         public void AddClient(Client client)
+         {
+            Clienti.Add(client);
+         }
+
+         //Sterge client din magazin
+         public void StergeClient(Client client)
+         {
+            Clienti.Remove(client);
+         }
+
+         //Procesare comanda in Magazin
+         public void ProcesareComanda(Client client)
+         {
+            // Procesare client
+         }
+
+
+
          //Pretul tuturor bicicletelor in magazin
          //In constructie
 
          public double GetPretTotal()
-         { 
-           return 0;
+         {
+            return 0;
          }
+      }
+   }
+
+   /// <summary>
+   /// Clasa Client initializata
+   /// </summary>
+   public class Client
+   {
+      public string Nume { get; set; }
+      public string Email { get; set; }
+      public string Telefon { get; set; }
+      public List<Bicicleta> Cos { get; set; }
+
+
+      //Initializare constructor pentru clasa client
+      public Client(string nume, string email, string telefon)
+      {
+         Nume = nume;
+         Email = email;
+         Telefon = telefon;
+         Cos = new List<Bicicleta>();
+      }
+
+      //Adaugare bicicleta in cos
+      public void AddCos(Bicicleta bicicleta)
+      {
+         Cos.Add(bicicleta);
+      }
+
+      //Stergere bicicleta din cos
+      public void StergeCos(Bicicleta bicicleta)
+      {
+         Cos.Remove(bicicleta);
+      }
+
+      public void Checkout()
+      {
+         // verifica cosul
       }
    }
 }
