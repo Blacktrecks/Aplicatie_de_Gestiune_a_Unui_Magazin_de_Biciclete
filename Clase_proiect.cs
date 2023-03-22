992fe6ae49f2eb20debb8566f9bc3179b9b7de11
Blacktrecks <107044395+Blacktrecks@users.noreply.github.com>
2023-03-22 23:39:13 +0200

    update project

--- Clase_proiect.cs
+++ Clase_proiect.cs
@@ -1,13 +1,14 @@
-﻿using System;
+﻿using Aplicatie_de_Gestiune_a_Unui_Magazin_de_Biciclete;
+using System;
 using System.Collections.Generic;
-
+using System.IO;
 
 
 namespace Aplicatie_de_Gestiune_a_Unui_Magazin_de_Biciclete
 {
    //Clasa Bicicleta reprezinta bicicleta cu urmatoarele caracteristici: brand, model, pret si disponibilitate pe stoc
 
-   class Bicicleta
+  public class Bicicleta
    {
       public string Brand { get; set; }
       public string Model { get; set; }
@@ -26,7 +27,7 @@ namespace Aplicatie_de_Gestiune_a_Unui_Magazin_de_Biciclete
       //Supradefinim si returnam variabilele clasei
       public override string ToString()
       {
-         return $"Brand: {Brand}, Model: {Model}, Pret: {Pret:C}, Available: {(Stoc ? "DA" : "NU")}";
+         return $"Brand: {Brand}, Model: {Model}, Pret: {Pret}RON, Disponibilitate: {(Stoc ? "DA" : "NU")}";
       }
 
       // Clasa MagazinBicicleta reprezinta un magazin online ce vinde biciclete
@@ -34,6 +35,15 @@ namespace Aplicatie_de_Gestiune_a_Unui_Magazin_de_Biciclete
       public class MagazinBicicleta
       {
          private List<Bicicleta> _biciclete = new List<Bicicleta>(); //declaram o noua lista privata ce va contine bicicletele
+         public List<Bicicleta> Inventar { get; set; }
+         public List<Client> Clienti { get; set; }
+
+         public MagazinBicicleta()
+         { 
+               Inventar = new List<Bicicleta>();
+               Clienti = new List<Client>();
+            
+         }
 
          //Adauga bicicleta 
          public void AdaugaBicicleta(Bicicleta bicicleta)
@@ -60,15 +70,226 @@ namespace Aplicatie_de_Gestiune_a_Unui_Magazin_de_Biciclete
                }
             }
             return bicicleteDisponibile.Count > 0 ? bicicleteDisponibile : new List<Bicicleta>();
-            
+
          }
+
+         //adauga bicicleta in magazin
+         public void AddBicicleta(Bicicleta bicicleta)
+         {
+            Inventar.Add(bicicleta);
+         }
+
+         //sterge bicicleta din magazin
+         public void StergeBicicleta(Bicicleta bicicleta)
+         {
+            Inventar.Remove(bicicleta);
+         }
+
+         //Adauga client in magazin
+         public void AddClient(Client client)
+         {
+            Clienti.Add(client);
+         }
+
+         //Sterge client din magazin
+         public void StergeClient(Client client)
+         {
+            Clienti.Remove(client);
+         }
+
+         //Procesare comanda in Magazin
+
+         //Procesarea comenziii clientului
+         public void ProceseazaComanda(Client clientComanda, Bicicleta bicicletaComanda)
+         {
+            bicicletaComanda.Stoc = false;
+            clientComanda.AddCos(bicicletaComanda);
+         }
+
+
          //Pretul tuturor bicicletelor in magazin
          //In constructie
 
          public double GetPretTotal()
-         { 
-           return 0;
+         {
+            double pretTotal = 0;
+            foreach (Bicicleta bicicleta in Inventar)
+            {
+               pretTotal += bicicleta.Pret;
+            }
+            return pretTotal;
          }
+
+         internal double GetPretTotalBiciclete()
+         {
+            double pretTotal = 0;
+
+            foreach (Bicicleta bicicleta in Inventar)
+            {
+               if (bicicleta.Stoc)
+               {
+                  pretTotal += bicicleta.Pret;
+               }
+            }
+            
+            return pretTotal;
+         }
+
+      }
+   }
+
+   /// <summary>
+   /// Clasa Client initializata
+   /// </summary>
+   public class Client
+   {
+      public string Nume { get; set; }
+      public string Email { get; set; }
+      public string Telefon { get; set; }
+      public List<Bicicleta> Cos { get; set; }
+
+
+      //Initializare constructor pentru clasa client
+      public Client(string nume, string email, string telefon)
+      {
+         Nume = nume;
+         Email = email;
+         Telefon = telefon;
+         Cos = new List<Bicicleta>();
+      }
+
+      //Adaugare bicicleta in cos
+      public void AddCos(Bicicleta bicicleta)
+      {
+         Cos.Add(bicicleta);
+      }
+
+      //Stergere bicicleta din cos
+      public void StergeCos(Bicicleta bicicleta)
+      {
+         Cos.Remove(bicicleta);
+      }
+
+      public void Checkout()
+      {
+         // verifica cosul
       }
    }
 }
+
+
+public class DataReader
+{
+   private readonly string _bicycleFilePath;
+   private readonly string _clientFilePath;
+
+   public DataReader(string bicycleFilePath, string clientFilePath)
+   {
+      _bicycleFilePath = bicycleFilePath;
+      _clientFilePath = clientFilePath;
+   }
+
+   public void PrintBicyclesAndClients()
+   {
+      var biciclete = ReadBicyclesFromFile(_bicycleFilePath);
+      var clienti = ReadClientsFromFile(_clientFilePath);
+      Console.WriteLine("Biciclete:");
+      foreach (var bicicleta in biciclete)
+      {
+         Console.WriteLine(bicicleta.ToString());
+      }
+
+      Console.WriteLine();
+
+      Console.WriteLine("Clienti:");
+      foreach (var client in clienti)
+      {
+      
+       Console.WriteLine($"Nume: {client.Nume}, Email: {client.Email}, Telefon: {client.Telefon}");
+         Console.WriteLine("Cos:");
+         foreach (var bicicleta in client.Cos)
+         {
+            Console.WriteLine(bicicleta.ToString());
+         }
+
+         Console.WriteLine();
+      }
+   }
+
+   private List<Bicicleta> ReadBicyclesFromFile(string filePath)
+   {
+      var biciclete = new List<Bicicleta>();
+
+      try
+      {
+         using (var reader = new StreamReader(filePath))
+         {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+               var bicycleData = line.Split(',');
+
+               var id = int.Parse(bicycleData[0]);
+               var model = bicycleData[1];
+               var brand = bicycleData[2];
+               var pret =  int.Parse(bicycleData[3]);
+               var disponibilitate =bool.Parse( bicycleData[4]);
+               
+
+               var bicicleta = new Bicicleta(model, brand,pret, disponibilitate);
+               biciclete.Add(bicicleta);
+            }
+         }
+      }
+      catch (Exception ex)
+      {
+         Console.WriteLine($"An error occurred while reading bicycles from file: {ex.Message}");
+      }
+
+      return biciclete;
+   }
+
+   private List<Client> ReadClientsFromFile(string filePath)
+   {
+      var clienti = new List<Client>();
+
+      try
+      {
+         using (var reader = new StreamReader(filePath))
+         {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+               var clientData = line.Split(';');
+
+               var nume = clientData[0];
+               var email = clientData[1];
+               var telefon = clientData[2];
+
+               var client = new Client(nume, email, telefon);
+               clienti.Add(client);
+
+               if (clientData.Length > 3)
+               {
+                  var cos = new List<Bicicleta>();
+                  for (int i = 3; i < clientData.Length; i++)
+                  {
+                     var bicycleData = clientData[i].Split(',');
+                     var bicycleId = int.Parse(bicycleData[0]);
+                     var bicycleQuantity = int.Parse(bicycleData[1]);
+
+                     //reminder nu functioneaza citirea clientilor din fisier
+                  }
+               }
+            }
+         }
+      }
+      catch (Exception ex)
+      {
+         Console.WriteLine($"An error occurred while reading clients from file: {ex.Message}");
+      }
+
+      return clienti;
+   }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,42 +1,157 @@
-﻿using Aplicatie_de_Gestiune_a_Unui_Magazin_de_Biciclete;
-using System;
-using System.Collections.Generic;
+﻿using System;
+using System.IO;
 using static Aplicatie_de_Gestiune_a_Unui_Magazin_de_Biciclete.Bicicleta;
 
-
-  class Program
+namespace Aplicatie_de_Gestiune_a_Unui_Magazin_de_Biciclete
 {
-   static void Main()
+   class Program
    {
-      MagazinBicicleta magazin = new MagazinBicicleta();
-
-      // Adaugare biciclete in magazin
-      magazin.AdaugaBicicleta(new Bicicleta("Montaje", "X3", 1899.99, true));
-      magazin.AdaugaBicicleta(new Bicicleta("Rampage", "Escape 5", 3450.99, false));
-      magazin.AdaugaBicicleta(new Bicicleta("Tokida", "Supreme JA", 6784.99, true));
-      
-
-      // Afisare biciclete din magazin
-
-      Console.WriteLine("Toate Bicicletele sunt:");
-      List<Bicicleta> biciclete = magazin.GetBicicleta();
-      foreach (Bicicleta bicicleta in biciclete)
+      static void Main(string[] args)
       {
-         Console.WriteLine(bicicleta);
+         MagazinBicicleta magazin = new MagazinBicicleta();
+
+         while (true)
+         {
+            Console.WriteLine("\nSelectati o optiune:");
+            Console.WriteLine("1 - Adauga bicicleta");
+            Console.WriteLine("2 - Sterge bicicleta");
+            Console.WriteLine("3 - Afiseaza lista biciclete");
+            Console.WriteLine("4 - Afiseaza stocul bicicletelor");
+            Console.WriteLine("5 - Adauga client");
+            Console.WriteLine("6 - Sterge client");
+            Console.WriteLine("7 - Proceseaza comanda");
+            Console.WriteLine("8 - Afiseaza pretul total al bicicletelor din magazin");
+            Console.WriteLine("9 - Citire si afisare date din fisier");
+            Console.WriteLine("0 - Iesire");
+
+            int option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+               case 1:
+                  Console.WriteLine("\nIntroduceti informatiile pentru bicicleta:");
+                  Console.Write("Brand: ");
+                  string brand = Console.ReadLine();
+                  Console.Write("Model: ");
+                  string model = Console.ReadLine();
+                  Console.Write("Pret: ");
+                  double pret = double.Parse(Console.ReadLine());
+                  Console.Write("Disponibilitate (DA/NU): ");
+                  bool stoc = Console.ReadLine().ToLower() == "da" ? true : false;
+
+                  Bicicleta bicicleta = new Bicicleta(brand, model, pret, stoc);
+                  magazin.AddBicicleta(bicicleta);
+
+                  Console.WriteLine("\nBicicleta a fost adaugata in magazin!");
+                  Console.WriteLine(bicicleta.ToString());
+                  break;
+
+               case 2:
+                  Console.WriteLine("\nIntroduceti informatiile pentru bicicleta de sters:");
+                  Console.Write("Brand: ");
+                  string brandToDelete = Console.ReadLine();
+                  Console.Write("Model: ");
+                  string modelToDelete = Console.ReadLine();
+
+                  Bicicleta bicicletaToDelete = magazin.Inventar.Find(b => b.Brand == brandToDelete && b.Model == modelToDelete);
+                  magazin.StergeBicicleta(bicicletaToDelete);
+
+                  Console.WriteLine("\nBicicleta a fost stearsa din magazin!");
+                  break;
+
+               case 3:
+                  Console.WriteLine("\nLista biciclete din magazin:");
+                  foreach (Bicicleta b in magazin.Inventar)
+                  {
+                     Console.WriteLine(b.ToString());
+                  }
+                  break;
+
+               case 4:
+                  Console.WriteLine("\nBicicletele disponibile din magazin:");
+                  foreach (Bicicleta b in magazin.GetBicicleteDisponibile())
+                  {
+                     Console.WriteLine(b.ToString());
+                  }
+                  break;
+
+               case 5:
+                  Console.WriteLine("\nIntroduceti informatiile pentru client:");
+                  Console.Write("Nume: ");
+                  string nume = Console.ReadLine();
+                  Console.Write("Email: ");
+                  string email = Console.ReadLine();
+                  Console.Write("Telefon: ");
+                  string telefon = Console.ReadLine();
+
+                  Client client = new Client(nume, email, telefon);
+                  magazin.AddClient(client);
+
+                  Console.WriteLine("\nClientul a fost adaugat in magazin!");
+                  Console.WriteLine(client.ToString());
+                  break;
+
+               case 6:
+
+                  Console.WriteLine("\nIntroduceti informatiile pentru clientul de sters:");
+                  Console.Write("Nume: ");
+                  string numeClient = Console.ReadLine();
+                  Console.Write("Email: ");
+                  string emailClient = Console.ReadLine();
+                  Console.Write("Telefon: ");
+                  string telefonClient = Console.ReadLine();
+
+                  Client clientToDelete = magazin.Clienti.Find(c => c.Nume == numeClient && c.Email == emailClient && c.Telefon == telefonClient);
+                  magazin.StergeClient(clientToDelete);
+
+                  Console.WriteLine("\nClientul a fost sters din magazin!");
+                  break;
+
+               case 7:
+                  Console.WriteLine("\nIntroduceti informatiile pentru comanda:");
+                  Console.Write("Nume client: ");
+                  string numeClientComanda = Console.ReadLine();
+                  Console.Write("Email client: ");
+                  string emailClientComanda = Console.ReadLine();
+                  Console.Write("Telefon client: ");
+                  string telefonClientComanda = Console.ReadLine();
+                  Console.Write("Brand bicicleta: ");
+                  string brandBicicletaComanda = Console.ReadLine();
+                  Console.Write("Model bicicleta: ");
+                  string modelBicicletaComanda = Console.ReadLine();
+
+                  Client clientComanda = magazin.Clienti.Find(c => c.Nume == numeClientComanda && c.Email == emailClientComanda && c.Telefon == telefonClientComanda);
+                  Bicicleta bicicletaComanda = magazin.Inventar.Find(b => b.Brand == brandBicicletaComanda && b.Model == modelBicicletaComanda);
+
+                  if (clientComanda == null)
+                  {
+                     Console.WriteLine("\nClientul nu exista in magazin!");
+                  }
+                  else if (bicicletaComanda == null)
+                  {
+                     Console.WriteLine("\nBicicleta nu exista in magazin!");
+                  }
+                  else
+                  {
+                     magazin.ProceseazaComanda(clientComanda, bicicletaComanda);
+                     Console.WriteLine("\nComanda a fost procesata!");
+                  }
+                  break;
+
+               case 8:
+                  Console.WriteLine($"\nPretul total al bicicletelor din magazin este: {magazin.GetPretTotalBiciclete()} RON");
+                  break;
+               case 9:
+                  var dataReader = new DataReader("biciclete.txt", "clienti.txt");
+                  dataReader.PrintBicyclesAndClients();
+                  break;
+               case 0:
+                  Console.WriteLine("\nLa revedere!");
+                  return;
+            }
+         }
       }
-      Console.WriteLine();
-
-      //Afisare biciclete disponibile pe stoc.
-
-      Console.WriteLine("Biciclete Disponibile:");
-      List<Bicicleta> bicicleteDisponibile = magazin.GetBicicleteDisponibile();
-      foreach (Bicicleta bicicleta in bicicleteDisponibile)
-      {
-         Console.WriteLine(bicicleta);
-      }
-      Console.WriteLine();
-
-      // Afisare pret total biciclete
-     // Console.WriteLine($"Pretul Total: {magazin.GetPretTotal():C}");
    }
 }
+
+
